a46ade58c1142ee3cf2936edbdea46eb0a340436
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-12-04 13:57:17 -0500

    added and finished read/write

---
+++ WriteReadLanaZE/WriteReadLanaZE/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WriteReadLanaZE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWrote = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(233, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "check if the strings within the file are equal";
+            // 
+            // lblWrote
+            // 
+            this.lblWrote.AutoSize = true;
+            this.lblWrote.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWrote.Location = new System.Drawing.Point(232, 380);
+            this.lblWrote.Name = "lblWrote";
+            this.lblWrote.Size = new System.Drawing.Size(355, 32);
+            this.lblWrote.TabIndex = 1;
+            this.lblWrote.Text = "output has been generated";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(348, 195);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(153, 66);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblWrote);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWrote;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ WriteReadLanaZE/WriteReadLanaZE/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WriteReadLanaZE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblWrote.Hide();
+        }
+
+        //Function: String Equalizer
+        //Input string str1, string str2
+        //Output: bool
+        //Desc: Compares strings inputted and sees if they're the same, caps excluded
+        private bool StringEqualizer(string str1, string str2)
+        {
+            //initialize bool to false
+            bool equal = false;
+
+            //checks length first for efficiency
+            if (str1.Length == str2.Length)
+            {
+                //convert the strings to upper so that caps are ignored
+                //this will not affect the actual strings
+                str1 = str1.ToUpper();
+                str2 = str2.ToUpper();
+
+                //if they are the same, set the bool to true
+                if (str1 == str2)
+                {
+                    equal = true;
+                }
+            }
+
+            //return the value
+            return equal;
+        }
+
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            //Read the input file into a string array
+            string[] file = System.IO.File.ReadAllLines(@"input.txt");
+
+            //array of chars I want gone
+            char[] charYeets = new char[] { ' ', '\t' };
+
+            //where my outputs will all go to
+            string output = "";
+
+            foreach (string line in file)
+            {
+                //split the lines of the file into two words
+                //there should only be two words in the file
+                string[] words = line.Split(charYeets, StringSplitOptions.RemoveEmptyEntries);
+
+                if (StringEqualizer(words[0], words[1]) == true)
+                {
+                    //output true, and add line break
+                    output += "true\r\n";
+                }
+                else
+                {
+                    //output false, and add line break
+                    output += "false\r\n";
+                }
+
+                //write the output to a new text file
+                System.IO.File.WriteAllText(@"output.txt", output);
+
+                lblWrote.Show();
+            }
+
+        }
+    
+    }
+}
