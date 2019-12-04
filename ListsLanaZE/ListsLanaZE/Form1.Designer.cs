4a7d85a72ad2f32ab8f22acc53cc7899fe4a4b42
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-12-04 11:13:13 -0500

    added and finished lists

---
+++ ListsLanaZE/ListsLanaZE/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ListsLanaZE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.txtMarks = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(130, 23);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 0;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.BtnEnter_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(553, 23);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 1;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.BtnAverage_Click);
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.ItemHeight = 16;
+            this.lstMarks.Location = new System.Drawing.Point(248, 66);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(275, 276);
+            this.lstMarks.TabIndex = 2;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(245, 396);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(47, 32);
+            this.lblAverage.TabIndex = 3;
+            this.lblAverage.Text = "??";
+            // 
+            // txtMarks
+            // 
+            this.txtMarks.Location = new System.Drawing.Point(12, 12);
+            this.txtMarks.Multiline = true;
+            this.txtMarks.Name = "txtMarks";
+            this.txtMarks.Size = new System.Drawing.Size(100, 39);
+            this.txtMarks.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMarks);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lstMarks);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnEnter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.TextBox txtMarks;
+    }
+}
+
---
+++ ListsLanaZE/ListsLanaZE/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListsLanaZE
+{
+    public partial class Form1 : Form
+    {
+        List<int> listMarks = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblAverage.Hide();
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int userMark = -1;
+
+            //convert value in textbox into int
+            try
+            {
+                userMark = int.Parse(txtMarks.Text);
+            }
+            catch (Exception parseError)
+            {
+                Console.WriteLine("An error occurred", parseError);
+                return;
+            }
+
+            //check if the user entered a number between 0 and 100
+            if (userMark >= 0 && userMark <= 100)
+            {
+                //add your mark to the list and listbox
+                lstMarks.Items.Add(userMark);
+                listMarks.Add(userMark);
+                lblAverage.Text = "Average: " + ListAverage(ref listMarks);
+            }
+        }
+
+        //Function: List Average
+        //Input: ref list
+        //Ouput: int
+        //Returns the average number of a inty list's contents
+        double ListAverage(ref List<int> listyMan)
+        {
+            //var 
+            double listAverage = 0;
+
+            //for every int in the list, add them up to listAverage 
+            for (int averageCount = 0; averageCount < listyMan.Count(); averageCount++)
+            {
+                listAverage += listyMan[averageCount];
+            }
+
+            //calculate the list's average number, round it, and return it to call
+            listAverage /= listyMan.Count();
+            Math.Round(listAverage, 2);
+            return listAverage;
+        }
+
+        private void BtnAverage_Click(object sender, EventArgs e)
+        {
+            lblAverage.Show();
+        }
+    }
+}
