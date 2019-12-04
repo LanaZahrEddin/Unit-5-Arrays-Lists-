45aae169b276407e7106508b4de6612ab3c3362b
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-12-03 21:28:41 -0500

    created and finished array into

---
+++ ArrayIntroLanaZE/ArrayIntroLanaZE/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ArrayIntroLanaZE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(350, 23);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(85, 387);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 1;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.BtnAverage_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 16;
+            this.lstNumbers.Location = new System.Drawing.Point(254, 63);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(267, 308);
+            this.lstNumbers.TabIndex = 2;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(543, 393);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(47, 32);
+            this.lblAverage.TabIndex = 3;
+            this.lblAverage.Text = "??";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
---
+++ ArrayIntroLanaZE/ArrayIntroLanaZE/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayIntroLanaZE
+{
+    public partial class Form1 : Form
+
+    {   //global vars and consts
+        const int MAX_ARRAY = 10;
+        Random numGen = new Random();
+
+        //make a new array
+        int[] numArray = new int[MAX_ARRAY];
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblAverage.Hide();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //local variables
+            int counter;
+            int randomNum;
+
+            //clear the listbox
+            lstNumbers.Items.Clear();
+
+            for (counter = 0; counter < numArray.Length; counter++)
+            {
+                //generate random number and add it to the array and the list
+                randomNum = numGen.Next(0, 9 + 1);
+                numArray[counter] = randomNum;
+                lstNumbers.Items.Add(randomNum);
+
+                //Hide the previous average
+                lblAverage.Hide();
+
+                //refresh the form
+                this.Refresh();
+            }
+
+        }
+
+        private void BtnAverage_Click(object sender, EventArgs e)
+        {
+            //local variables
+            double average;
+            double sum = 0;
+            int counter;
+
+            //calculate the sum of the numebrs in the array
+            for (counter = 0; counter < numArray.Length; counter++)
+            {
+                sum += numArray[counter];
+            }
+
+            //calculate and display the average
+            average = sum / numArray.Length;
+            lblAverage.Text = "Average: " + average;
+            lblAverage.Show();
+
+        }
+    }
+}
