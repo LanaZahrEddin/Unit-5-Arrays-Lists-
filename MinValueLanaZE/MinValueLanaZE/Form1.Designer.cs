a0057c4506eeaec8c400d8fdc7834e334be3ed8a
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-12-03 22:52:00 -0500

    added and finished min value

---
+++ MinValueLanaZE/MinValueLanaZE/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MinValueLanaZE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMinValue = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblMinValue
+            // 
+            this.lblMinValue.AutoSize = true;
+            this.lblMinValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinValue.Location = new System.Drawing.Point(258, 397);
+            this.lblMinValue.Name = "lblMinValue";
+            this.lblMinValue.Size = new System.Drawing.Size(47, 32);
+            this.lblMinValue.TabIndex = 0;
+            this.lblMinValue.Text = "??";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(341, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 16;
+            this.lstNumbers.Location = new System.Drawing.Point(255, 76);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(246, 276);
+            this.lstNumbers.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblMinValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMinValue;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+    }
+}
+
---
+++ MinValueLanaZE/MinValueLanaZE/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinValueLanaZE
+{
+    public partial class Form1 : Form
+    {   
+        //consts and vars
+        const int MAX_ARRAY = 10;
+        const int MAX_VALUE = 500;
+        int[] intyBois = new int[MAX_ARRAY];
+        Random numGen = new Random(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblMinValue.Hide();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+
+            //variables
+            int minValue, counter, randomNum;
+
+            //clear the list
+            lstNumbers.Items.Clear();
+
+            for (counter = 0; counter < intyBois.Length; counter++)
+            {
+                //generates a random number, adds it to the list and array
+                randomNum = numGen.Next(0, MAX_VALUE);
+                intyBois[counter] = randomNum;
+                lstNumbers.Items.Add(randomNum);
+            }
+
+            //find the min value using the funky ting
+            minValue = MinValue(intyBois);
+
+            //display the label and the min value
+            lblMinValue.Show();
+            lblMinValue.Text = "Min Value: " + minValue;
+
+        }
+
+        //Function: MinValue
+        //Input: int[] array
+        //Output: int
+        //Description: finds the smallest value within an array
+        private int MinValue(int[] array)
+        {
+            //assign the minvalue to the first number in the array
+            int minValue = array[0];
+
+            //if any number is smaller than the minValue,
+            //then if there's a number in the array that's smaller
+            //minValue gets replaced
+            foreach (int inty in array)
+            {
+                if (inty < minValue)
+                {
+                    minValue = inty;
+                }
+            }
+
+            return minValue;
+        }
+    }
+}
