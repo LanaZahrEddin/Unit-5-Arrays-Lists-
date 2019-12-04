c4590d99ba55df83d364ac82afc8135bf970c54a
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-12-03 22:02:38 -0500

    added and finished find max value

---
+++ MaxValueLanaZE/MaxValueLanaZE/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MaxValueLanaZE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(361, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 16;
+            this.lstNumbers.Location = new System.Drawing.Point(252, 55);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(301, 308);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.AutoSize = true;
+            this.lblMaxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaxValue.Location = new System.Drawing.Point(355, 389);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(47, 32);
+            this.lblMaxValue.TabIndex = 2;
+            this.lblMaxValue.Text = "??";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMaxValue);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblMaxValue;
+    }
+}
+
---
+++ MaxValueLanaZE/MaxValueLanaZE/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxValueLanaZE
+{
+    public partial class Form1 : Form
+    {
+        //global vars and consts
+        const int MAX_ARRAY = 10;
+        const int MAX_VALUE = 500;
+        int[] intyBoi = new int[MAX_ARRAY];
+        Random numGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblMaxValue.Hide();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //local vars and consts
+            int counter, randomNum, maxValue;
+
+            //clear the items within the list
+            lstNumbers.Items.Clear();
+
+            for (counter = 0; counter < intyBoi.Length; counter++)
+            {
+                //generate a random number, then add it to the list and the array
+                randomNum = numGen.Next(0, MAX_VALUE + 1);
+                lstNumbers.Items.Add(randomNum);
+                intyBoi[counter] = randomNum;
+            }
+
+            //use the funky ting
+            maxValue = MaxValue(intyBoi);
+
+            //show the label, and display the max value
+            lblMaxValue.Text = "Max Value: " + maxValue;
+            lblMaxValue.Show();
+
+        }
+
+        //Function: MaxValue
+        //Input: int[] array
+        //Output: int
+        //Desc: finds the highest value within an array
+        private int MaxValue(int[] array)
+        {
+            //variables
+            int maxValue = array[0];
+
+            //checks all the numbers in the array
+            //if it is bigger than the first value
+            //change maxValue to be that number
+            foreach (int value in array)
+            {
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                }
+            }
+
+            return maxValue;
+        }
+
+    }
+}
